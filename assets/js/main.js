aaf7190f6de23968bf0c1e3d9bb0c30ca450b6a8
Kateryna Solovei <soloveikat07@gmail.com>
2023-04-08 14:19:48 -0400

    hw jasmine done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,93 @@ class Mat {
     this.a = a;
     this.b = b;  
   }
+
   sum() {
     return this.a + this.b;
   }
+  
   sub() {
     return this.a - this.b;
   }
 }
+
+
+// Age Classification
+
+function ageClassification(num) {
+  return num > 0 ?
+    num <= 24 ? "детский возраст" :
+      num <= 44 ? "молодой возраст" :
+        num <= 65 ? "средний возраст" :
+          num <= 75 ? "пожилой возраст" :
+            num <= 90 ? "старческий возраст" :
+              num <= 122 ? "долгожители" :
+                null : null;
+}
+
+console.log("-1 :", ageClassification(-1)); // -1 : null
+console.log("5 :", ageClassification(5)); // 5 : детский возраст
+console.log("34 :", ageClassification(34)); // 34 : молодой возраст
+console.log("50 :", ageClassification(50)); // 50 : средний возраст
+console.log("65.1 :", ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log("80 :", ageClassification(80)); // 80 : старческий возраст
+console.log("110 :", ageClassification(110)); // 110 : долгожители
+console.log("130 :", ageClassification(130));
+
+
+console.log("    -1 :", ageClassification(-1)); // -1 : null
+console.log("     1 :", ageClassification(1)); // 1 : детский возраст
+console.log("    24 :", ageClassification(24)); // 24 : детский возраст
+console.log(" 24.01 :", ageClassification(24.01)); // 24.01 : молодой возраст
+console.log("    44 :", ageClassification(44)); // 44 : молодой возраст
+console.log(" 44.01 :", ageClassification(44.01)); // 44.01 : средний возраст
+console.log("    65 :", ageClassification(65)); // 65 : средний возраст
+console.log("  65.1 :", ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log("    75 :", ageClassification(75)); // 75 : пожилой возраст
+console.log(" 75.01 :", ageClassification(75.01)); // 75.01 : старческий возраст
+console.log("    90 :", ageClassification(90)); // 90 : старческий возраст
+console.log(" 90.01 :", ageClassification(90.01)); // 90.01 : долгожители
+console.log("   122 :", ageClassification(122)); // 122 : долгожители
+console.log("122.01 :", ageClassification(122.01)); // 122.01 : null
+console.log("   130 :", ageClassification(130)); // 130 : null
+
+
+//Day of the week
+function weekFn(w) {
+  let str = "";
+
+  switch (w) {
+    case 1:
+      str = "Понедельник";
+      break;
+    case 2:
+      str = "Вторник";
+      break;
+    case 3:
+      str = "Среда";
+      break;
+    case 4:
+      str = "Четверг";
+      break;
+    case 5:
+      str = "Пятница";
+      break;
+    case 6:
+      str = "Суббота";
+      break;
+    case 7:
+      str = "Воскресенье";
+      break;
+    default:
+      str = null;
+  }
+
+  return str;
+}
+
+console.log(weekFn(1)); // 'Понедельник'
+console.log(weekFn(3)); // 'Среда'
+console.log(weekFn(7)); // 'Воскресенье'
+console.log(weekFn(9)); // null
+console.log(weekFn(1.5)); // null
+console.log(weekFn("2")); // null
--- main.spec.js
+++ main.spec.js
@@ -1,9 +1,68 @@
-describe("name test#1", function () {
-  let a;
 
-  it("check a=true", function () {
-    a = true;
+describe("Age Classification", () => {
 
-    expect(a).toBe(true);
+  it("returns null if age is less than or equal to 0", () => {
+    expect(ageClassification(-1)).toBe(null);
+    expect(ageClassification(0)).toBe(null);
+  });
+
+  it("returns 'детский возраст' if age is less than or equal to 24", () => {
+    expect(ageClassification(1)).toBe("детский возраст");
+    expect(ageClassification(24)).toBe("детский возраст");
+  });
+
+  it("returns 'молодой возраст' if age is less than or equal to 44", () => {
+    expect(ageClassification(24.01)).toBe("молодой возраст");
+    expect(ageClassification(44)).toBe("молодой возраст");
+  });
+
+  it("returns 'средний возраст' if age is less than or equal to 65", () => {
+    expect(ageClassification(44.01)).toBe("средний возраст");
+    expect(ageClassification(65)).toBe("средний возраст");
+  });
+
+  it("returns 'пожилой возраст' if age is less than or equal to 75", () => {
+    expect(ageClassification(65.1)).toBe("пожилой возраст");
+    expect(ageClassification(75)).toBe("пожилой возраст");
+  });
+
+  it("returns 'старческий возраст' if age is less than or equal to 90", () => {
+    expect(ageClassification(75.01)).toBe("старческий возраст");
+    expect(ageClassification(90)).toBe("старческий возраст");
+  });
+
+  it("returns 'долгожители' if age is less than or equal to 122", () => {
+    expect(ageClassification(90.01)).toBe("долгожители");
+    expect(ageClassification(122)).toBe("долгожители");
+  });
+
+  it("returns null if age is greater than 122", () => {
+    expect(ageClassification(122.01)).toBe(null);
+    expect(ageClassification(130)).toBe(null);
+  });
+
+});
+
+
+describe("Days of the week", function() {
+  it("return day of the week", function() {
+    expect(weekFn(1)).toBe("Понедельник");
+  });
+  it("return day of the week", function() {
+    expect(weekFn(3)).toBe("Среда");
+  });
+  it("return day of the week", function() {
+    expect(weekFn(7)).toBe("Воскресенье");
+  });
+  it("return day of the week", function() {
+    expect(weekFn(9)).toBe(null);
+ });
+ it("return day of the week", function() {
+    expect(weekFn(1.5)).toBe(null);
+  });
+  it("return day of the week", function() {
+    expect(weekFn("2")).toBe(null);
   });
 });
+
+
